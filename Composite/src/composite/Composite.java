378e32e1f84a26692f80da35f477b024931ce77d
sonkkeli <laurilasonja@gmail.com>
2019-10-24 20:53:58 +0300

    project for task 3

---
+++ Composite/src/composite/Composite.java
@@ -0,0 +1,16 @@
+package composite;
+
+/**
+ *
+ * @author sonja
+ */
+public class Composite {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
